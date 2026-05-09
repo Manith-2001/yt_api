64dec5c8d9d7f281432f2e79ecf52fb7b8ab9284
Manith-2001 <manith.vazirani@pec.edu>
2026-05-09 23:11:28 +0530

    changed : handler spawns a new thread per connection

--- handlers/download_handler.c
+++ handlers/download_handler.c
@@ -1,6 +1,11 @@
 #include "../mongoose/mongoose.h"
+#include "../threads/api_thread.h"
 #include "../threads/job_queue.h"
+#include <_string.h>
+#include <_time.h>
+#include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 void data_handler(struct mg_connection *c, struct mg_http_message *hm) {
   char *link = mg_json_get_str(hm->body, "$.link");
@@ -9,9 +14,16 @@ void data_handler(struct mg_connection *c, struct mg_http_message *hm) {
     mg_http_reply(c, 400, "", "{\"error\": \"missing 'link' field\"}\n");
     return;
   }
-  enque_download(link);
-
-  mg_http_reply(c, 200, "", "{\"received\": \"%s\"}\n", link);
-  mg_free(link);
+  // enque_download(link);
+  void *fd = c->fd;
+  c->fd = (void *)MG_INVALID_SOCKET;
+  c->is_closing = 1;
+  WorkerArgs *args = calloc(1, sizeof(WorkerArgs));
+  args->fd = fd;
+  args->link = link;
+  pthread_t t;
+  pthread_create(&t, NULL, api_function, args);
+  pthread_detach(t);
+  // mg_http_reply(c, 200, "", "{\"received\": \"%s\"}\n", link);
   return;
 }
