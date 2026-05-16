d9ad55afe6c0b50a425a81bfb4edbd27ddd995e9
Manith-2001 <manith.vazirani@pec.edu>
2026-05-16 23:25:36 +0530

    changed : api thread listens to completed calls

--- threads/api_thread.c
+++ threads/api_thread.c
@@ -1,23 +1,26 @@
 #include "api_thread.h"
+#include "job_queue.h"
 #include <_time.h>
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/_pthread/_pthread_t.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
 void *api_function(void *arg) {
   WorkerArgs *args = arg;
+  pthread_t tid = pthread_self();
+  printf("Spun api thread of id : %d", *(int *)tid);
   int sockfd = (int)(intptr_t)args->fd;
-
+  enque_download(args->link, *(int *)tid);
   // Send headers once — no Content-Length, chunked encoding
   const char *headers = "HTTP/1.1 200 OK\r\n"
-                        "Content-Type: application/json\r\n"
                         "Transfer-Encoding: chunked\r\n"
-                        "Connection: keep-alive\r\n"
-                        "\r\n";
-  send(sockfd, headers, strlen(headers), MSG_NOSIGNAL);
+                        "Content-Type: video/mp4\r\n"
+                        "Connection: close\r\n\r\n";
+  send(sockfd, headers, strlen(headers), 0);
 
   // Keep sending chunks until client disconnects
   char *json_body =
@@ -29,8 +32,20 @@ void *api_function(void *arg) {
                              strlen(json_body), json_body);
 
     int n = send(sockfd, chunk, chunk_len, MSG_NOSIGNAL);
-    if (n <= 0)
+    if (n <= 0) {
+      printf("client gone\n");
       break; // client gone
+    }
+    completed_msg c_msg = dequeu_completed();
+    printf("Got link : %s of id : %d\n", c_msg.link, c_msg.tid);
+    if (c_msg.tid == *(int *)tid) {
+      printf("my link breaking\n");
+      // free(c_msg.link);
+      break;
+    } else {
+      printf("not myu link queing again\n");
+      enque_completed(c_msg.link, c_msg.tid);
+    }
     sleep(1);
   }
 
