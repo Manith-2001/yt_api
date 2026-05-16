14cac4c9a5ae27406f79dacbcda50d61cd4c2409
Manith-2001 <manith.vazirani@pec.edu>
2026-05-16 23:24:30 +0530

    added : completed queue

--- threads/job_queue.c
+++ threads/job_queue.c
@@ -2,8 +2,11 @@
 #include <_string.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/_pthread/_pthread_t.h>
+#include <sys/fcntl.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
+#include <unistd.h>
 
 key_t download_key;
 int downloadqueue_id;
@@ -11,26 +14,58 @@ key_t complete_key;
 int completequeue_id;
 
 void queue_init(void) {
+
+  close(open("downloadqueue", O_CREAT | O_WRONLY, 0644));
+  close(open("completedqueue", O_CREAT | O_WRONLY, 0644));
   download_key = ftok("downloadqueue", 65);
   downloadqueue_id = msgget(download_key, 0666 | IPC_CREAT);
+  complete_key = ftok("completedqueue", 65);
+  completequeue_id = msgget(complete_key, 0666 | IPC_CREAT);
 }
 
-void enque_download(char *link) {
+void enque_download(char *link, int tid) {
   download_msg d_msg;
   d_msg.mtype = 1;
   size_t len = strlen(link);
   strcpy(d_msg.link, link);
-  if (msgsnd(downloadqueue_id, &d_msg, sizeof(d_msg.link), 0) == -1) {
+  d_msg.tid = tid;
+  if (msgsnd(downloadqueue_id, &d_msg, sizeof(download_msg) - sizeof(long),
+             0) == -1) {
     // TDOO : handle error sending message
     printf("Error sending message to download queue\n");
   }
 }
 
-char *dequeu_download(void) {
+download_msg dequeu_download(void) {
   download_msg r_msg;
-  if (msgrcv(downloadqueue_id, &r_msg, sizeof(r_msg.link), 1, 0) == -1) {
+  if (msgrcv(downloadqueue_id, &r_msg, sizeof(download_msg) - sizeof(long), 1,
+             0) == -1) {
     perror("msgrcv");
   }
-  printf("Message received: %s\n", r_msg.link);
-  return strdup(r_msg.link);
+  printf("Download Message received: %s\n", r_msg.link);
+  printf("Download ID received: %d\n", r_msg.tid);
+  return r_msg;
+}
+
+void enque_completed(char *link, int tid) {
+  completed_msg c_msg;
+  c_msg.mtype = 1;
+  size_t len = strlen(link);
+  strcpy(c_msg.link, link);
+  c_msg.tid = tid;
+  if (msgsnd(completequeue_id, &c_msg, sizeof(completed_msg) - sizeof(long),
+             0) == -1) {
+    // TDOO : handle error sending message
+    printf("Error sending message to download queue\n");
+  }
+}
+
+completed_msg dequeu_completed(void) {
+  completed_msg r_msg;
+  if (msgrcv(completequeue_id, &r_msg, sizeof(completed_msg) - sizeof(long), 1,
+             0) == -1) {
+    perror("msgrcv");
+  }
+  printf("Completed Message received: %s\n", r_msg.link);
+  return r_msg;
 }
--- threads/job_queue.h
+++ threads/job_queue.h
@@ -1,12 +1,22 @@
 #pragma once
+#include "pthread.h"
 
 #define MAX_LINK_LEN 1024
 
 typedef struct {
   long mtype;
   char link[MAX_LINK_LEN];
+  int tid;
 } download_msg;
 
+typedef struct {
+  long mtype;
+  char link[MAX_LINK_LEN];
+  int tid;
+} completed_msg;
+
 void queue_init(void);
-void enque_download(char *link);
-char *dequeu_download(void);
+void enque_download(char *link, int tid);
+download_msg dequeu_download(void);
+void enque_completed(char *link, int tid);
+completed_msg dequeu_completed(void);
